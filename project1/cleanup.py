34f7c3ad59ff5512c5b7497988e826ed858526bd
yellowsimulator <yellowsimulator@gmail.com>
2020-10-09 15:08:51 +0200

    p

--- project1/cleanup.py
+++ project1/cleanup.py
@@ -3,7 +3,7 @@ import pandas as pd
 import warnings
 warnings.filterwarnings("ignore")
 
-
+"""
 def get_exel_sheet_names(input_file_name):
     sheet_objects = xlrd.open_workbook(input_file_name, on_demand=True)
     sheet_list = sheet_objects.sheet_names()
@@ -13,3 +13,5 @@ def get_exel_sheet_names(input_file_name):
 def get_data_frame_from_excel(input_file_name, sheet_name):
     df = pd.read_excel(input_file_name, sheet_name=sheet_name, index_col=0)
     return df
+"""
+
